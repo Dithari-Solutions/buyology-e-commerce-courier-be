cf74a379bc4afaef8c63055079125e421d3cc057
Firdovsirz <firdovsirz@gmail.com>
2026-04-17 15:59:25 +0400

    feat: courier proofs

--- src/test/java/com/buyology/buyology_courier/BuyologyCourierApplicationTests.java
+++ src/test/java/com/buyology/buyology_courier/BuyologyCourierApplicationTests.java
@@ -2,8 +2,16 @@ package com.buyology.buyology_courier;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "contabo.s3.endpoint=http://localhost:9000",
+        "contabo.s3.access-key=test",
+        "contabo.s3.secret-key=test",
+        "contabo.s3.bucket-name=test-bucket",
+        "contabo.s3.public-url=http://localhost:9000/test-bucket"
+})
 class BuyologyCourierApplicationTests {
 
 	@Test
