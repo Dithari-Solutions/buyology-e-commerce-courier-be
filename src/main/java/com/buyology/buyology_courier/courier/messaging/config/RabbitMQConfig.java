c8fee8d5434a7ad89ba4d93030c51d80bbca197c
Firdovsirz <firdovsirz@gmail.com>
2026-04-27 17:32:31 +0400

    message broker

--- src/main/java/com/buyology/buyology_courier/courier/messaging/config/RabbitMQConfig.java
+++ src/main/java/com/buyology/buyology_courier/courier/messaging/config/RabbitMQConfig.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -114,7 +114,9 @@ public class RabbitMQConfig {
 
     @Bean
     MessageConverter jsonMessageConverter() {
-        return new JacksonJsonMessageConverter();
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setAlwaysConvertToInferredType(true);
+        return converter;
     }
 
     @Bean
